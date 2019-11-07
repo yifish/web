1b4ef39f4e97de2b34e6a768f989f197f042fcce
xiaoyu <xiaoyu@chujiax.com>
2019-11-07 11:42:19 +0800

    配置

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+//按需加载,当渲染其他页面时才加载其组件,并缓存,减少首屏加载时间
+const index = resolve => require(['@/view/index.vue'], resolve)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '首页',
+      component: index
     }
   ]
 })
